bbdb227df20a0adb833a5326c097ac817bf1813b
zhangtianzong <legendztz@outlook.com>
2025-02-16 11:08:40 -0600

    add analysis

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Analytics />
       </body>
+      <Analytics />
     </html>
   );
 }
